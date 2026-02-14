7902dfbcd3d0eae16961d00f5cf90800ec8833f1
wce-acm <wceacmsc@gmail.com>
2026-02-15 01:21:28 +0530

    final changes added

--- frontend/src/Pages/MachineCard.jsx
+++ frontend/src/Pages/MachineCard.jsx
@@ -157,7 +157,7 @@ export default function MachineCard({ machine, onClick }) {
         </div>
 
         {/* ================= MARK DELIVERED BUTTON ================= */}
-        {machine.progress === 100 &&
+        {/* {machine.progress === 100 &&
           machine.status === 'COMPLETED' && (
             <div className="mt-4">
               <button
@@ -172,7 +172,7 @@ export default function MachineCard({ machine, onClick }) {
                 {loading ? 'Marking...' : 'Mark Delivered'}
               </button>
             </div>
-          )}
+          )} */}
 
       </div>
 
